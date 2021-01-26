0d04ce43a9790d1cd9d36b3c336413f08271d08c
YUAN21 <1650192445@qq.com>
2021-01-26 15:59:34 +0800

    peace init

---
+++ packages/utils/lib/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import Func from './src/func'
+import * as WebAPI from './src/webapi'
+import { AuthorizationCode } from './src/authCode'
+import { PinyinHelper } from './src/pinyin'
+import ActionHelper from './src/actionHelper'
+import ReducerHelper from './src/reducerHelper'
+import Constans from './src/constans'
+
+export {
+    Func,
+    WebAPI,
+    AuthorizationCode,
+    PinyinHelper,
+    ActionHelper,
+    ReducerHelper,
+    Constans
+}
