11d6e85500788997e927b0201935b00d681de4b7
YUAN21 <1650192445@qq.com>
2021-01-27 11:37:29 +0800

    add components package

---
+++ packages/components/lib/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import Form from './form'
+import ImageCropper from './imageCropper'
+import Modal from './modal'
+import MultiSelect from './multiSelect'
+import Search from './search'
+import Spin from './spin'
+import Table from './table'
+
+export {
+    ImageCropper,
+    MultiSelect,
+    Form,
+    Modal,
+    Search,
+    Table,
+    Spin
+}
