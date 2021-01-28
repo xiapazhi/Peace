dd8f33b4fcc92d03683f49aa0904f7e59940e848
YUAN21 <1650192445@qq.com>
2021-01-28 09:51:58 +0800

    add RegionTree

--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -6,8 +6,9 @@ import { PinyinHelper } from './src/pinyin'
 import ActionHelper from './src/actionHelper'
 import ReducerHelper from './src/reducerHelper'
 import Constans from './src/constans'
+import Region from './src/region'
 
-export { Func, WebAPI, PinyinHelper, ActionHelper, ReducerHelper, Constans }
+export { Func, WebAPI, PinyinHelper, ActionHelper, ReducerHelper, Constans, Region }
 
 import { buildUrl, buildRoute, Request, RouteRequest } from './src/webapi'
 export { buildUrl, buildRoute, Request, RouteRequest }
