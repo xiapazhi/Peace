9de11c50cc25f3c4f4b0992a7aa5e662b521878e
YUAN21 <1650192445@qq.com>
2021-02-05 11:27:49 +0800

    add Colors

--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -8,8 +8,9 @@ import ReducerHelper from './src/reducerHelper'
 import Constans from './src/constans'
 import Region from './src/region'
 import { sort } from './src/smartSort'
+import * as Colors from './src/colors'
 
-export { Func, WebAPI, PinyinHelper, ActionHelper, ReducerHelper, Constans, Region as region, sort }
+export { Func, WebAPI, PinyinHelper, ActionHelper, ReducerHelper, Constans, Region as region, sort, Colors }
 
 import { buildUrl, buildRoute, Request, RouteRequest } from './src/webapi'
 export { buildUrl, buildRoute, Request, RouteRequest }
