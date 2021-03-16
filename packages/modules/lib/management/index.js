38e64349ba1a3db1e063058de0a02c5c8baceb50
YUAN21 <1650192445@qq.com>
2021-03-16 09:55:14 +0800

    以发布

--- packages/modules/lib/management/index.js
+++ packages/modules/lib/management/index.js
@@ -4,7 +4,7 @@ import routes from './routes';
 import reducers from './reducers';
 import actions from './actions';
 import {getNavItem} from './containers/nav-item';
-
+//
 export default {
     key: 'management',
     name: '系统管理',
